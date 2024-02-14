66868f0cb14b0dcd8d71b4ed1a7df170c4bb4b3d
Vaanmathi24 <159941824+Vaanmathi24@users.noreply.github.com>
2024-02-14 13:46:30 +0530

    Update StudentController.java

--- src/main/java/com/example3/database2/StudentController.java
+++ src/main/java/com/example3/database2/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/api")
 public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
